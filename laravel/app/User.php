10ffc048dfd695f41f0e943dc18ac0241413b875
IvanII <ivanoviaa92@gmail.com>
2018-04-09 01:30:08 +0600

    Added relation with coupon

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Models\Coupon;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -26,4 +27,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Relation with coupon
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function coupons()
+    {
+        return $this->hasMany(Coupon::class);
+    }
 }
