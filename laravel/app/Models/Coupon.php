0cb93dc27fe83439e38cc99ac1eecae088caec32
IvanII <ivanoviaa92@gmail.com>
2018-04-09 01:29:42 +0600

    Added methods for obtain and release coupons

--- laravel/app/Models/Coupon.php
+++ laravel/app/Models/Coupon.php
@@ -2,9 +2,49 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Coupon extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id'];
+
+    /**
+     * Find first coupon where user_id is null and set give it user
+     *
+     * @param $userId
+     * @return bool
+     */
+    public static function obtain($userId)
+    {
+        return self::whereNull('user_id')
+                ->first()
+                ->update(['user_id' => $userId]);
+    }
+
+    /**
+     * Find all coupons where user_id is not null and set null to user_id property
+     *
+     * @return bool|int
+     */
+    public static function release()
+    {
+        return self::whereNotNull('user_id')
+                ->update(['user_id' => null]);
+    }
+
+    /**
+     * Relation with user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
